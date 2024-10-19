71c0791ac5ef461aca6d76783d1048888f432a23
Sean Yang <seanyang12345@gmail.com>
2024-10-19 16:05:50 -0700

    slakdjf

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,11 @@
-<<<<<<< Updated upstream
 import OriginalGraph from '../components/OriginalGraph';
-=======
 import SearchBox from '@/components/SearchBox';
-import NetworkGraph from '../components/NetworkGraph';
->>>>>>> Stashed changes
 
 export default function Home() {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
-<<<<<<< Updated upstream
-    <OriginalGraph />
-=======
     <SearchBox />
-    <NetworkGraph />
->>>>>>> Stashed changes
+    <OriginalGraph />
   </div>
   );
 }
