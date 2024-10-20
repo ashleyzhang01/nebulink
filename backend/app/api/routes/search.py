e1335ff21cd9f3429cea8e17fd133c55f00fb037
“kellyhongsn” <kellyhongsn@berkeley.edu>
2024-10-19 19:07:08 -0700

    done with search

--- backend/app/api/routes/search.py
+++ backend/app/api/routes/search.py
@@ -10,6 +10,7 @@ from app.chromadb.internal_api import query_from_chroma
 from app.db.linkedin_organization_functions import get_linkedin_organization_by_id
 from app.db.linkedin_user_functions import get_users_by_organization
 from app.models.linkedin_organization import LinkedinOrganization
+from app.models.repository import Repository
 from app.schemas.linkedin_organization import LinkedinOrganization as LinkedinOrganizationSchema
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
@@ -19,6 +20,8 @@ from typing import Dict, List
 from app.schemas.search import GeneralQuerySchema, LinkedinQuerySchema
 from app.utils.enums import ChromaCollections
 from app.utils.github_scraper import GithubScraper
+from backend.app.db.github_user_functions import get_github_users_by_repository
+from backend.app.db.repository_functions import get_repository_by_path
 
 
 router = APIRouter(prefix="/network", tags=["network"])
@@ -30,19 +33,31 @@ async def general_search(
     db: Session = Depends(get_db)
 ):
     classification = classify_query_func(query=query)
+    github_results: list[Repository] = []
+
     if classification == "other":
         optimized_query = handle_other_query_func(query=query)
-        chroma_client = get_chroma_collection(collection=ChromaCollections.GITHUB_REPOSITORY)
-        # TODO: github case
+        github_chroma_results: list[ChromaResult] = query_from_chroma(
+            query=optimized_query,
+            n_results=5,
+            collection=ChromaCollections.GITHUB_REPOSITORY,
+        )
+
+        for github_chroma_result in github_chroma_results:
+            github_results.append(
+                get_repository_by_path(
+                    path=github_chroma_result.id,
+                    db=db,
+                )
+            )
         
-    
-    # Linkedin case
     optimized_query = handle_company_query_func(query=query)
     chroma_results: list[ChromaResult] = query_from_chroma(
         query=optimized_query,
         n_results=5,
         collection=ChromaCollections.LINKEDIN_ORGANIZATION,
     )
+
     linkedin_results: list[LinkedinOrganization] = []
     for chroma_result in chroma_results:
         linkedin_results.append(
@@ -52,8 +67,7 @@ async def general_search(
             )
         )
 
-
-    # Query from chroma using the classification and the optimized query
+    return GeneralQuerySchema(linkedin_results=linkedin_results, github_results=github_results)
     
 
 
